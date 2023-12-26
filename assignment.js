287c30b6baf1ae87a3a703550790eb530755cf56
Louis Gauthron <louis@matlux.net>
2023-12-26 17:21:25 +0000

    first commit

---
+++ assignment.js
@@ -0,0 +1,36 @@
+/*
+function describeCountry(country, population, capitalCity) {
+    const description = `${country} has ${population} million people and it's capital city is ${capitalCity}`
+    return description;
+}
+
+const England = describeCountry(`england`, 67, `london`);
+console.log(England);
+const France = describeCountry(`france`, 64, `paris`);
+console.log(France);
+const Russia = describeCountry(`russia`, 144, `moscow`);
+console.log(Russia);
+
+function percentageOfWorld1(population) {
+    return (population / 7900) * 100
+}
+const China = percentageOfWorld1(1441)
+console.log(China);
+
+const percentageOfWorld2 = function (population) {
+    return (population / 7900) * 100
+}
+const China2 = percentageOfWorld2(1441)
+console.log(China2)
+
+const percentageOfWorld3 = population => (population / 7900) * 100;
+const China3 = percentageOfWorld3(1441);
+console.log(China3);
+
+function describePopulation(country, population) {
+    const china4 = percentageOfWorld1(1441)
+    const description2 = `${country} has ${population} million people which is ${china4}% of the worlds population!`
+    return description2
+}
+console.log(describePopulation('China', 1441))
+*/
---
+++ script.js
@@ -0,0 +1,98 @@
+'use strict';
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log('I can drive ;D');
+
+// const interface = 'Audio';
+// const private = 534;
+
+function logger() {
+    console.log('My name is Jonas');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4)
+console.log(appleOrangeJuice);
+
+const num = Number(`23`);
+
+// Function declaration
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+
+// Function expression
+const calcAge2 = function (birthyear) {
+    return 2037 - birthyear
+}
+const age2 = calcAge2(1991);
+
+console.log(age1, age2)
+
+// Arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsAntilRetirment = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirment = 65 - age;
+    // return retirment;
+    return `${firstName} retires in ${retirment} years`;
+}
+
+console.log(yearsAntilRetirment(1991, 'Jonas'));
+console.log(yearsAntilRetirment(1980, 'Bob'));
+
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `juice with ${applePieces} apple pieces and ${orangePieces} orange pieces.`;
+    return juice;
+}
+console.log(fruitProcessor(2, 3));
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const yearsAntilRetirment = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirment = 65 - age;
+
+    if (retirment > 0) {
+        console.log(`${firstName} retires in ${retirment} years`);
+        return retirment;
+    } else {
+        console.log(`${firstName} has already retired!!`)
+        return -1;
+    }
+
+
+}
+
+console.log(yearsAntilRetirment(1991, 'Jonas'));
+console.log(yearsAntilRetirment(1950, 'Mike'));
+*/
